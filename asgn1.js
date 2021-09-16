452eb8939d77583a6e6987c1068c0ef152d8896d
Maxfield Kassel <mekassel@wisc.edu>
2021-09-16 18:22:36 -0500

    Finished drawDice and added draw function

--- asgn1.js
+++ asgn1.js
@@ -1,26 +1,31 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 
+var colors = ["red","orange","yellow","green","blue","purple"]
+
+var frameCount = 0;
+var scene = 1;
+
+/*
+Draws a circle on the canvas of a set size
+*/
+function drawCircle(x, y, size, fill){
+    context.moveTo(x, y);
+    context.arc(x, y, size, Math.PI, 0);
+    context.arc(x, y, size, 0, Math.PI);
+}
+
 /*
 Draws a dice on the canvas from point (x, y) on a 
 canvas to its size, with the dice number specified.
-todo: fix scaling, add switch statement to for dice number
-! the red surrounding is the size of the dice, this needs to be removed later
+The color of the dice number can 
 */
-function Draw_Dice( x,  y,  size, number) {
-    context.strokeStyle = "red";
-    context.lineWidth = 1;
-    context.beginPath();
-    context.moveTo(x,y);
-    context.lineTo(x,y + size);
-    context.lineTo(x + size,y + size);
-    context.lineTo(x + size,y);
-    context.lineTo(x,y);
-    context.stroke();
+function drawDice( x,  y,  size, number, color) {
     //Draw outer dice
     context.strokeStyle = "black"; 
     context.lineWidth = Math.sqrt(size)/5;
     var diceScale = size/4;
+    var circleSize = diceScale/3;
     context.beginPath();
     context.arc(x + diceScale, y + diceScale, diceScale,  -Math.PI , -Math.PI / 2, false);
     context.arc(x + size - diceScale, y + diceScale, diceScale,  -Math.PI / 2, 0, false);
@@ -28,6 +33,74 @@ function Draw_Dice( x,  y,  size, number) {
     context.arc(x + diceScale, y + size - diceScale, diceScale, Math.PI / 2, - Math.PI, false);
     context.closePath();
     context.stroke();
+    //Draw numbers on dice
+    context.strokeStyle = "black"; 
+    context.fillStyle = color;
+    context.lineWidth = Math.sqrt(size)/10;
+    
+    context.beginPath();
+    switch(number) {
+        case 1: 
+            drawCircle(x + size/2, y + size/2, circleSize);
+            break;
+        case 2: 
+            drawCircle(x + size/10 * 7, y + size/10 * 7, circleSize);
+            drawCircle(x + size/10 * 3, y + size/10 * 3, circleSize);
+            break;
+        case 3: 
+            drawCircle(x + size/10 * 8, y + size/10 * 2, circleSize);
+            drawCircle(x + size/10 * 5, y + size/10 * 5, circleSize);
+            drawCircle(x + size/10 * 2, y + size/10 * 8, circleSize);
+            break;
+        case 4: 
+            drawCircle(x + size/4, y + size/4, circleSize);
+            drawCircle(x + size/4 * 3, y + size/4, circleSize);
+            drawCircle(x + size/4, y + size/4 * 3, circleSize);
+            drawCircle(x + size/4 * 3, y + size/4 * 3, circleSize);
+            break;
+        case 5: 
+            drawCircle(x + size/4, y + size/4, circleSize);
+            drawCircle(x + size/4 * 3, y + size/4, circleSize);
+            drawCircle(x + size/4, y + size/4 * 3, circleSize);
+            drawCircle(x + size/4 * 3, y + size/4 * 3, circleSize);
+            drawCircle(x + size/2, y + size/2, circleSize);
+            break;
+        case 6: 
+            for(var i = 1; i < 4; i++) {
+                drawCircle(x + size/4, y + (size/10) * (3 * i - 1), circleSize);
+                drawCircle(x + size/4 * 3, y + (size/10) * (3 * i - 1), circleSize);
+            }
+            break;
+    }
+    context.stroke();
+    context.fill();  
+}  
+
+/*
+* Draw the dice background!
+*/
+function drawChangingDiceBackground(){
+    for(var i = 0; i < 6; i++) {
+        for(var j = 0; j < 6; j++)
+            drawDice(i*canvas.width/6,j * canvas.width/6,canvas.width/6,(frameCount+j+i)%6+1, colors[(j+i)%6]);
+    }
 }
 
-Draw_Dice(100,100,50);
+function drawFirstScene(){
+
+}
+
+function draw(){
+     context.clearRect(0,0,canvas.width,canvas.height);
+     switch(scene) {
+         case 0:
+            drawChangingDiceBackground();
+        break;
+         case 1:
+            drawChangingDiceBackground();
+        break;
+     }
+    frameCount++;
+}
+
+setInterval(draw, 200);
