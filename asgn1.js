b7e7e014daf1a14e8809d3f6194b7cc8bf41f94b
Maxfield Kassel <mekassel@wisc.edu>
2021-09-18 16:21:07 -0500

    Added setShape

--- asgn1.js
+++ asgn1.js
@@ -2,14 +2,9 @@ var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 
 
-
-var frameCount = 0;
-var physicsUpdateCount = 0;
-var scene = 0;
-
-
 const targetFrameRate = 10;
 const physicsUpdateRate = 100;
+
 
 //Got inspiration for shapes from:
 //https://en.wikipedia.org/wiki/Tetromino
@@ -28,6 +23,7 @@ const startXBoard = 20;
 const startYBoard = 20;
 const boardRows = 24;
 const boardCols = 10;
+
 var board = new Array(boardCols);
 for(var i = 0; i < boardCols;i++){
     board[i] = new Array(boardRows);
@@ -35,10 +31,13 @@ for(var i = 0; i < boardCols;i++){
         board[i][j] = -1;
     }
 }
-board[0][1] = 1;
-board[0][2] = 2;
-board[4][15] = 3;
-board[8][5] = 4;
+
+var frameCount = 0;
+var physicsUpdateCount = 0;
+var currentHeight = 0;
+var currentPose = boardCols/2;
+var currentPhysicsUpdates = 0;
+var updateSpeed = 10; //Physics frames per update. 
 
 //Found how to create classes here:
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
@@ -96,8 +95,8 @@ class Shape {
 }
 
 function setColor(color){
-    context.strokeStyle=color;
-    context.fillStyle = color;
+    context.strokeStyle = colors[color];
+    context.fillStyle = colors[color];
 }
 
 function drawGrid(x, y, height, width) {
@@ -123,13 +122,14 @@ function drawSquare(x, y) {
     context.lineTo(x, y+scale);
     context.closePath();
     context.fill();
+    context.stroke();
 }
 
 function drawBoard(){
     for(var row = 0; row < boardRows; row++) {
         for (var col = 0; col < boardCols; col++) {
             if(board[col][row] != -1) {
-                setColor(colors[board[col][row]-1])
+                setColor(board[col][row]);
                 drawSquare(startXBoard + col * scale, startYBoard + row * scale);
             }
         }
@@ -139,41 +139,88 @@ function drawBoard(){
 
 function draw(){
     context.clearRect(0,0,canvas.width,canvas.height);
-    movingShape.drawShape(startXBoard+slider1.value*scale,startYBoard+Math.floor(physicsUpdateCount/40)*scale);
+    movingShape.drawShape(startXBoard+currentPose*scale,startYBoard+(currentHeight*scale));
     drawBoard();
     //movingShape.rotateLeft();
     
-    
-    
-    
-     switch(scene) {
-         case 0:
-          
-        break;
-         case 1:
-            
-        break;
-     }
     frameCount++;
 }
-
+function canMove(shape, x, y) {
+    if(currentHeight + shape.height >= boardRows)
+        return false;
+    for(var i = 0; i < movingShape.getSquares().length; i++) {
+        if(board[movingShape.getSquares()[i][0]+x][movingShape.getSquares()[i][1]+y] != -1) {
+            return false;
+        }
+    }
+    return true;
+}
+function setShape(shape) {
+   for(var i = 0; i < shape.getSquares().length; i++) {
+        board[shape.getSquares()[i][0]+currentPose][shape.getSquares()[i][1]+currentHeight] = shape.color;
+    }
+}
 function fixedUpdate(){
-    
+    if(canMove(movingShape,parseInt(slider1.value),currentHeight)) {
+        currentPose = parseInt(slider1.value);
+    }
+    if(currentPhysicsUpdates >= updateSpeed) {
+        currentPhysicsUpdates = 0;
+        
+
+        if(!canMove(movingShape,currentPose,currentHeight+1)) {
+            setShape(movingShape);
+            movingShape = new Shape(parseInt(Math.random()*7), parseInt(Math.random()*5));
+            currentHeight = 0;
+            if(!canMove(movingShape,currentPose,currentHeight)) {
+                console.log("GameOver");
+            }
+        } else {
+            currentHeight++;
+        }
+
+        
+
+        
+    }
+
+
+    //Set the sliders maximum value to 10 - the width of the moving shape.
+    //This ensures that the user cannot put the piece off of the board.
     //Found how to set attribute here: 
     //http://help.dottoro.com/ljgaxrgj.php
     slider1.setAttribute ("max", 10-movingShape.width);
     physicsUpdateCount++;
+    currentPhysicsUpdates++;
 }
 
+// ! issue with rotating objects and causing objects to spawn outside of grid
 function rotateLeft(){
+    var width = movingShape.width;
     movingShape.rotateLeft();
+    if(movingShape.width > width) {
+        currentPose = movingShape.width - width;
+    }
+    if(!canMove(movingShape,currentPose,currentHeight)) {
+        movingShape.rotateRight();
+    }
 }
 function rotateRight(){
+    var width = movingShape.width;
     movingShape.rotateRight();
+    var difference = movingShape.width - width;
+    if(difference > 0) {
+        
+        currentPose = currentPose - difference;
+        slider1.value = slider1.value - difference;
+    }
+    if(!canMove(movingShape,currentPose,currentHeight)) {
+        movingShape.rotateLeft();
+    }
 }
 
 
-var movingShape = new Shape(6,"red");
+var movingShape = new Shape(6,2);
 setInterval(fixedUpdate, 10);
 setInterval(draw, 200);
 leftButton.addEventListener('click', rotateLeft);
