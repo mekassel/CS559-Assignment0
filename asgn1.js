5c0bdd4115c5266e86e7b14907ae88d23daaaeda
mekassel <mekassel@wisc.edu>
2021-09-18 06:40:18 -0500

    Working on adding physics to shapes.

--- asgn1.js
+++ asgn1.js
@@ -1,34 +1,73 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 
-var colors = ["red","orange","yellow","green","blue","purple"]
+
 
 var frameCount = 0;
+var physicsUpdates = 0;
 var scene = 0;
-var scale = 20;
-var   shapes = [
+
+const targetFrameRate = 10;
+const physicsUpdateRate = 100;
+
+//Got inspiration for shapes from:
+//https://en.wikipedia.org/wiki/Tetromino
+const shapes = [
     [[[0,0],[0,1],[1,0],[1,1]]], //Square
     [[[0,0],[1,0],[2,0],[3,0]],[[0,0],[0,1],[0,2],[0,3]]], //Line
-    [[0,0],[0,1],[0,2],[1,2]], //L shape
+    [[[0,0],[0,1],[0,2],[1,2]]], //L shape
 ];
+const colors = ["red","orange","yellow","green","blue","purple"]
+const scale = 20;
+const startXBoard = 20;
+const startYBoard = 20;
+const boardRows = 20;
+const boardCols = 10;
+var board = new Array(boardCols);
+for(var i = 0; i < boardCols;i++){
+    board[i] = new Array(boardRows);
+    for(var j = 0; j < boardRows; j++) {
+        board[i][j] = -1;
+    }
+}
+board[0][1] = 1;
+board[0][2] = 2;
+board[4][15] = 3;
+board[8][5] = 4;
 
 //Found how to create classes here:
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
 class Shape {
-
+    height;
+    width;
   
    constructor(type, color) {
     this.squares = shapes[type];
     this.color = color;
     this.rotationState = 0;
+    this.updateWidthAndHeight();
   }
 
+
+    getSquares() {
+        return this.squares[this.rotationState];
+    }
+
     drawShape(x, y) {
-        context.strokeStyle=this.color;
-        context.fillStyle = this.color;
-        for(var i = 0; i < this.squares[this.rotationState].length;i++) {
-            drawSquare(x + scale*this.squares[this.rotationState][i][0],y + scale*this.squares[this.rotationState][i][1]);
+        setColor(this.color);
+        for(var i = 0; i < this.getSquares().length;i++) {
+            drawSquare(x + scale*this.getSquares()[i][0],y + scale*this.getSquares()[i][1]);
         }
+   }
+
+    updateWidthAndHeight() {
+        this.width = 0;
+        this.height = 0;
+        for(var i = 0; i < this.getSquares().length;i++) {
+            this.width = Math.max(this.width,this.getSquares()[i][0]+1);
+            this.height = Math.max(this.height,this.getSquares()[i][1]+1);
+        }
+        return 0;
    }
 
    rotateLeft(){
@@ -36,13 +75,24 @@ class Shape {
         if(this.rotationState < 0) {
             this.rotationState = this.squares.length - 1;
         }
+        this.updateWidthAndHeight();
+   }
+
+   rotateRight() {
+       this.rotationState++;
+        if(this.rotationState > this.squares.length-1) {
+            this.rotationState = 0;
+        }
+        this.updateWidthAndHeight();
    }
 
   
-   getWidth() {
-       var width = 0;
+  
+}
 
-   }
+function setColor(color){
+    context.strokeStyle=color;
+    context.fillStyle = color;
 }
 
 function drawGrid(x, y, height, width) {
@@ -69,16 +119,31 @@ function drawSquare(x, y) {
     context.closePath();
     context.fill();
 }
+
+function drawBoard(){
+    drawGrid(startXBoard,startYBoard,boardRows,boardCols,scale);
+
+    for(var row = 0; row < boardRows; row++) {
+        for (var col = 0; col < boardCols; col++) {
+            if(board[col][row] != -1) {
+                setColor(colors[board[col][row]-1])
+                drawSquare(startXBoard + col * scale, startYBoard + row * scale);
+            }
+        }
+    }
+}
 var movingShape = new Shape(1,"red");
 function draw(){
      context.clearRect(0,0,canvas.width,canvas.height);
-    
-    movingShape.drawShape(20,20);
-    movingShape.rotateLeft();
+    console.log("UPDATE");
+    movingShape.drawShape(startXBoard+slider1.value*scale,startYBoard+Math.floor(frameCount/2)*scale);
+    drawBoard();
+    //movingShape.rotateLeft();
     //Found how to set attribute here: 
     //http://help.dottoro.com/ljgaxrgj.php
-    slider1.setAttribute ("max", 6);
-    drawGrid(20,50,20,10,20);
+    
+    
+    
      switch(scene) {
          case 0:
           
@@ -90,5 +155,18 @@ function draw(){
     frameCount++;
 }
 
+function fixedUpdate(){
+    //slider1.value = 1;
+    slider1.setAttribute ("max", 10-movingShape.width);
+}
 
+function rotateLeft(){
+    movingShape.rotateLeft();
+}
+function rotateRight(){
+    movingShape.rotateLeft();
+}
+setInterval(fixedUpdate, 10);
 setInterval(draw, 200);
+leftButton.addEventListener('click', rotateLeft);
+rightButton.addEventListener('click', rotateRight);
